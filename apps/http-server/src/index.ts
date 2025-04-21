4114f7c072a314292cce5f983b2a8bb591263021
Aditya Raj <you@example.com>
2025-04-21 20:00:57 +0530

    added-http-layer

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,12 +1,124 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cors from "cors";
 import { prisma } from "@repo/db";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { authMiddleware } from "./authMiddleware";
+import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types"
+
+declare global {
+  namespace Express {
+      export interface Request {
+          userId?:string;
+      }
+  }
+}
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.json{
+      msg: "incorrect inputs"
+    }
+    return;
+  }
+
+  const { email, password, name } = req.body;
+
+  const userExists = await prisma.user.findFirst({
+    where: {
+      email
+    }
+  });
+  if (userExists) {
+    res.json({ success: false, msg: "User already exists" });
+    return;
+  }
+
+  try {
+    const hashedPass = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPass,
+        name
+      }
+    })
+
+    res.json({ success: true, msg: "User created" });
+  } catch (error) {
+    res.json({ success: false, msg: "Error while signup "})
+  }
+})
+
+app.post("/signin", async (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json{
+      msg: "incorrect inputs"
+    }
+    return;
+  }
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email
+      }
+    })
+    if (!user) {
+      res.json({ success: false, msg: "User doesn't exist...please signup"});
+      return;
+    }
+    const passMatch = bcrypt.compare(password, user.password);
+    if (!passMatch) {
+      res.json({ success: false, msg: "Incorrect Password"});
+      return;
+    }
+    const token = jwt.sign({
+      userId: user.id
+    }, JWT_SECRET);
+    res.json({ success: true, msg: "User logged in", token});
+  } catch (error) {
+    res.json({success: false, msg: "error while login"})
+  }
+})
+
+app.post("/room", authMiddleware, async (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json{
+      msg: "incorrect inputs"
+    }
+    return;
+  }
+  const { slug } = req.body;
+  const userId = req.userId || "";
+
+  try {
+    const room = await prisma.room.create({
+      data: {
+        slug,
+        adminId: userId
+      }
+    })
+  
+    res.json({
+      roomId: room.id
+    })
+  } catch (error) {
+    res.json({
+      msg: "room need to be unique"
+    })
+  }
+})
 
 app.listen(PORT, () => {
     console.log(`Server runing at http://localhost:${PORT}`);
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,10 +1,32 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken"
 import { prisma } from "@repo/db";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket) => {
-    socket.on("message", (msg) => {
-        socket.send("pong");
+wss.on("connection", function connection(ws, request) {
+    // authorization
+    const url = request.url;
+    if (!url) {
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get("token") || "";
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (typeof decoded == "string") {
+        ws.close();
+        return;
+    }
+
+    if (!decoded || !decoded.userId) {
+        ws.close();
+        return;
+    }
+
+    ws.on("message", function message(data) {
+        ws.send("pong");
     })
 })
